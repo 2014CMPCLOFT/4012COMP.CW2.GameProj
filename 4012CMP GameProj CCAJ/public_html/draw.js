65acc329ab0fc282f5f0e5c17a213ca4608b13ae
2014CMPCLOFT <c.loftus@2013.ljmu.ac.uk>
2014-02-26 15:46:07 +0000

    Moved some files around
    Images go in an images folder

--- 4012CMP GameProj CCAJ/public_html/draw.js
+++ 4012CMP GameProj CCAJ/public_html/draw.js
@@ -1,1 +1,19 @@
 //Animation and rendering each object
+
+    var WIDTH = 650;
+    var HEIGHT = 850;
+
+var stage = new Kinetic.Stage({
+    container: 'game',
+    height: HEIGHT,
+    width: WIDTH
+});
+
+var layer = new Kinetic.Layer();
+
+var rect = new Kinetic.Rect({
+    x: this.WIDTH/2,
+    y: this.HEIGHT/2,
+    fill: 'red'
+});
+
--- 4012CMP GameProj CCAJ/public_html/init.js
+++ 4012CMP GameProj CCAJ/public_html/init.js
@@ -1,30 +1,26 @@
-//Main loop, variables and the instructions for the other script files
+//Main loop and the instructions for the other script files
 //Note that it turns out you can't get nested with Javascript without trying out
 // something I don't think we'll practice in tutorials, so to be on the safe side
 // we'll just include all the .js files on the HTML
     
-    //var canvas; //Not used?
-    var ctx;
-    var WIDTH = 400;
-    var HEIGHT = 300;
-    var intervalId = 0;
-
-
 //NB: Two animation methods, revise to decide which one we will use
 function init() {
     /*Ucanvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");                 //1st Anim Method
     return setInterval(draw, 10);*/
-    ctx = $('#Game')[0].getContext("2d");          //2nd Anim Method
+    /*ctx = $('#Game')[0].getContext("2d");          //2nd Anim Method
     intervalId = setInterval(draw, 10);
-    }
+    }*/
     
-//Mouse integration
+    //NEW SHINY KINECTJS ANIMATION METHOD
+    var anim = new Kinetic.Animation(function(frame) {
+    var time = frame.time,
+        timeDiff = frame.timeDiff,
+        frameRate = frame.frameRate;
 
-    var canvasMinX = 0;
-    var canvasMaxX = 0;
+    // update stuff here
     
-    function init_mouse() {
-    canvasMinX = $("#Game").offset().left;
-    canvasMaxX = canvasMinX + WIDTH;
-    }
+  }, layer);
+
+  anim.start();
+  }
